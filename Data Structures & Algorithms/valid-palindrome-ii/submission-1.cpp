61bf2267c2442b55d139b71ecf977dae764e62ca
Suchethan Vilasagaram <suchethanvilasagaram@gmail.com>
2026-05-15 13:06:23 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool ispal(string s,int i,int j){
+        int n = s.size();
+       
+        
+        while(i < j) {
+            if(s[i]!=s[j]){
+                return false;
+                
+
+            }
+            
+                i++;
+                j--;
+            
+
+        }
+            return true; 
+    }
+    bool validPalindrome(string s) {
+        
+        int n = s.size();
+        int i = 0,j = n-1;
+        int k = 1;
+        while(i < j) {
+            if(s[i]!=s[j]){
+                return ispal(s,i+1,j) || ispal(s,i,j-1);
+                
+
+            }
+            
+                i++;
+                j--;
+            
+
+        }
+            return true;    
+    }
+};
