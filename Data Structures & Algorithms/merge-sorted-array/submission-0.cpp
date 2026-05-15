b0d4abf22a74fe4352bf4f2ff28ae26ebc9a1618
Suchethan Vilasagaram <suchethanvilasagaram@gmail.com>
2026-05-15 21:06:23 +0530

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+
+        //nums1.resize(m+n);
+        int i = m,j = 0;
+        while( j < n){
+            nums1[i++] = nums2[j++];
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
