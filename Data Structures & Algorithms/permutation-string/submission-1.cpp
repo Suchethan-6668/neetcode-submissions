eca963717f292d32246efd64d6a6aadebf1fb609
Suchethan Vilasagaram <suchethanvilasagaram@gmail.com>
2026-06-23 16:40:48 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n1 = s1.size();
+        int n2 = s2.size();
+        int j = 0;
+        unordered_map<char,int> mp1;
+        for(char &c : s1) {
+            mp1[c]++;
+        }
+        unordered_map<char,int> mp2;
+        for(int i = 0;i < n2;i++) {
+            if(i >= n1) {
+                mp2[s2[j]]--;
+                if(mp2[s2[j]] == 0)mp2.erase(s2[j]);
+                j++;
+            }
+            mp2[s2[i]]++;
+            if((i-j+1) == n1) {
+                if(mp1 == mp2) {
+                    return true;
+                }
+            }
+            
+        }
+        return false;
+    }
+};
