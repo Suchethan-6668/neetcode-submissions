dbee8cfa3bb73ae69c3d41619cd13c8602d7d015
Suchethan Vilasagaram <suchethanvilasagaram@gmail.com>
2026-07-03 19:04:38 +0530

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<int> dp(target+1,0);
+        dp[0] = 1;
+
+
+        for(int i = 1;i <= target;i++) {
+            for(int &j : nums) {
+                
+               if(i >= j)dp[i] += dp[i-j];
+            }
+        }
+        return dp[target];
+    }
+};
