2924adaa312ae1ef76091b7ee1993bf20ba8fbff
Suchethan Vilasagaram <suchethanvilasagaram@gmail.com>
2026-07-27 18:17:00 +0530

    Add: combinations - submission-2

---
+++ Data Structures & Algorithms/combinations/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void solve(int ind,int n,int k,vector<int> &curr,vector<vector<int>> &ans) {
+        if(curr.size() == k)ans.push_back(curr);
+        for(int i = ind;i <= n;i++) {
+            if(curr.size() >=k)continue;
+            curr.push_back(i);
+            solve(i+1,n,k,curr,ans);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        solve(1,n,k,curr,ans);
+        return ans;
+    }
+};
