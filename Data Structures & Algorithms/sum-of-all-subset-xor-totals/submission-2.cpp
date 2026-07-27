d9d89d3b23f48ae359411360da0851d959974ecf
Suchethan Vilasagaram <suchethanvilasagaram@gmail.com>
2026-07-27 16:41:07 +0530

    Add: sum-of-all-subset-xor-totals - submission-2

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(int ind,int &sum,int curr,vector<int> &nums,int n) {
+        if(ind == n){sum += curr;return;}
+        solve(ind+1,sum,curr,nums,n);
+        
+        solve(ind+1,sum,curr^nums[ind],nums,n);
+
+    }
+    int solve(int ind,int curr,vector<int> &nums,int n) {
+        if(ind == n){return curr;}
+        return solve(ind+1,curr,nums,n)+solve(ind+1,curr^nums[ind],nums,n);
+
+    }
+    int subsetXORSum(vector<int>& nums) {
+        int n = nums.size();
+
+        int sum = 0;
+
+        //solve(0,sum,0,nums,n);
+        return solve(0,0,nums,n);
+        return sum;
+
+
+    }
+};
